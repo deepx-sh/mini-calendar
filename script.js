75678073f4ea1be5772f6912b673cc6bba5b8a89
Deep Prajapati <dvprajapati7212@gmail.com>
2025-07-10 21:47:33 +0530

    changes in script file

---
+++ script.js
@@ -0,0 +1,13 @@
+let currenDate = new Date();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let date = document.querySelector(".date");
+let day = document.querySelector(".day");
+let month = document.querySelector(".month");
+let year = document.querySelector(".year");
+
+date.innerHTML = `${currenDate.getDate()}`;
+day.innerHTML = `${days[currenDate.getDay()]}`;
+month.innerHTML = `${monthArr[currenDate.getMonth()]}`
+year.innerHTML=`${currenDate.getFullYear()}`
